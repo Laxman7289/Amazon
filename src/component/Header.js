d2532d26d30a638fffc5b9107b592be8083f8608
Laxman7289 <127031359+Laxman7289@users.noreply.github.com>
2023-03-23 21:26:03 +0530

    add

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,6 +11,7 @@ import './Header.css'
 const fadeImages = [
     {
       url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      
     },
     {
       url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
@@ -63,7 +64,7 @@ function NavScrollExample() {
       </Container>
     </Navbar>
 
-
+ 
     <div className="slide-container">
       <Fade>
         {fadeImages.map((fadeImage, index) => (
