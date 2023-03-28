fd22b9b62126ecac7b2d45052749a0443b452a4c
Laxman7289 <127031359+Laxman7289@users.noreply.github.com>
2023-03-28 06:00:23 +0000

    Add files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import NavScrollExample from './component/Header'
 import 'react-slideshow-image/dist/styles.css'
 import Product from './component/Product'
+import Home from './component/Home'
+import Link from './component/Link'
+
 
 // import Header from './component/Header'
 // import { Add } from './try'
@@ -13,7 +17,21 @@ const App = () => {
     <NavScrollExample/>
 
     <Product/>
+
+< BrowserRouter>
+    <Routes>
+      <Route path="/action1" element={<Home/>} />
+      <Route path="/action2" element={<Link/>} />
+
+    </Routes>
+
+</BrowserRouter>
+
+
+
     </>
+
+    
   )
 }
 
